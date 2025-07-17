553800d5173b0be1e3de722172a9f4c349b20233
rajesh-gurajala <gurajalarajesh006@gmail.com>
2025-07-17 12:26:33 +0530

    app.py has included auth_routes

--- prompt_gen/app.py
+++ prompt_gen/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from models.orm_models import Base
 from database.db import engine
-from api.routes import router
+from api.routes import router as user_router
 from api.prompt.prompt_routes import router as prompt_router
+from api.auth.auth_routes import app as auth_router
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-app.include_router(router)
+app.include_router(user_router)
 app.include_router(prompt_router)
+app.include_router(auth_router)
