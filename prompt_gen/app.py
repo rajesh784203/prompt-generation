50a7c3c52c0b40825737b090c66d57bf624ae94e
rajesh-gurajala <gurajalarajesh006@gmail.com>
2025-07-20 11:08:07 +0530

    added prefix /api to all routes in app.py

--- prompt_gen/app.py
+++ prompt_gen/app.py
@@ -17,6 +17,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(user_router)
-app.include_router(prompt_router)
-app.include_router(auth_router)
+app.include_router(user_router, prefix="/api")
+app.include_router(prompt_router, prefix="/api")
+app.include_router(auth_router, prefix="/api")
